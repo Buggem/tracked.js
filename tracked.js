8e833d05b6603d8a3f4d66cf1800b7630100b328
Max Parry <66670928+Buggem@users.noreply.github.com>
2022-10-30 14:38:54 +1100

    Create tracked.js

---
+++ tracked.js
@@ -0,0 +1,9 @@
+(function() {
+  window.tracked = {
+    'location': Intl.DateTimeFormat().resolvedOptions().timeZone.split('/').map(function(s) {
+      return s.split('_').join(' ');
+    } );
+    'canRead': navigator.language.toLowerCase(),
+    
+  };
+})();
